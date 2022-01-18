22c9600e02edbadc0b479232172215a94b84990f
Aleksei Liakhov <Aleksei_Liakhov@epam.com>
2022-01-18 08:03:17 +0300

    First commit from Intelij IDEA

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("All systems are working!!!");
+        System.out.println("So, what are we doing now!!!");
+        System.out.println("I can do this!!!");
+        System.out.println("I will do this!!!");
+        System.out.println("So, just do it!!!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println(" ");
+
+        int i = 9;
+        while (i > -1){
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+
+}
